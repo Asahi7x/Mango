a450bebbea7f3d4effb5910b1d422f827bcc63d0
Sator1 <Christian@dev.dev>
2026-09-20 22:09:19 -0500

    Creamos el asignar rol en el auth service, falta el controlador

--- Mango.Services.AuthAPI/Services/AuthService.cs
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -25,6 +25,23 @@ namespace Mango.Services.AuthAPI.Services
             _tokenGenerator = tokenGenerator;
         }
 
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = await _context.ApplicationUsers.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+
+            if(user != null)
+            {
+                if(!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    //Creamos el rol si no existe
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user,roleName);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             var user = await _context.ApplicationUsers.FirstOrDefaultAsync(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
--- Mango.Services.AuthAPI/Services/IServices/IAuthService.cs
+++ Mango.Services.AuthAPI/Services/IServices/IAuthService.cs
@@ -5,7 +5,7 @@ namespace Mango.Services.AuthAPI.Services.IServices
     public interface IAuthService
     {
         Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
-
         Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<bool> AssignRole(string email, string roleName);
     }
 }
